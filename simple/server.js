751eff862aa03eaf8bd1dc11cd2a71277ef18706
Cihat Kösem <ctk.cihatksm@gmail.com>
2024-09-21 11:19:03 +0300

    Refactor server.js to use res.json() for sending JSON response

--- simple/server.js
+++ simple/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { UseRender, SchemeManager, Config } = require('../server');
+const { UseRender, SchemeManager, Config, ConfigKeys } = require('../server');
 const app = express()
 const port = 3000;
 
@@ -7,15 +7,21 @@ const schemes = new SchemeManager();
 schemes.load(__dirname + '/schemes');
 
 const config = new Config();
-config.set('views', __dirname + '/html');
 
-app.use((req) => { req.default_scheme = 'app'; req.next(); });
+// If you want to use the default scheme, you can set the scheme name to the DEFAULT_SCHEME key. 
+config.set(ConfigKeys.VIEWS, __dirname + '/html');
+
+// If you want to use the default scheme, you can set the scheme name to the DEFAULT_SCHEME key.
+config.set(ConfigKeys.DEFAULT_SCHEME, 'app');
+
+// If you want to ignore errors while rendering HTML files, you can set the value to true.
+config.set(ConfigKeys.IGNORE_ERRORS, true);
+
 app.use(UseRender);
 
 app.get('/', (req, res) => {
     res.renderer('index', { title: 'Express' });
 })
-
 
 app.post('/', (req, res) => {
     console.log('Posted');
